10ac8ce86a09d692fa761f029138f5890726c596
Livia Helen <liviasilva.diventech@gmail.com>
2022-11-20 20:33:34 -0300

    Create password generator with pop-up.

---
+++ script.js
@@ -0,0 +1,33 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#size");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()_+";
+
+let newPass = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
+
+function generatePassword() {
+  let pass = "";
+  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  newPass = pass;
+}
+
+function clipboardPassword() {
+  navigator.clipboard.writeText(newPass);
+  toastr.success("Copiado para a área de transferência");
+}
